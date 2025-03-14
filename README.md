c86627ce7dc5b01b1c213d18480be9f34bdea643
Deep Coder <120001510+deep017i@users.noreply.github.com>
2025-03-14 20:07:14 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+### Insta Reel Saver Bot
+- Advanced Bot
+- Best UI
+- Advenced Download Mode
+- Advanced Floodwait Mode
+- Enable Floodwait Mode
+- Force Subscribe
+- Easy to Deploy 
+- Database Added
+- Ban / Unban &  Broadcast Feature
+- Multi Fsub Feature
+
+## Deploy On
+- Seenode (Paid)
+- koyeb (Free)
+- Render (Free)
+- Heroku (Paid)
+
+## Build Command
+```
+pip install -r requirements.txt
+```
+
+## Start Command
+```
+gunicorn app:app & python3 bot.py
+```
+
+## Developer
+- This Repo Design, coded, Develop & modified by [Dypixx](https://dypixx.vercel.app).
---
+++ var.py
@@ -0,0 +1,20 @@
+#The repo is fully coded and modified by @Dypixx.
+#Please do not sell or remove credits.
+
+import os
+
+API_ID = os.getenv("API_ID", "22532891")
+API_HASH = os.getenv("API_HASH", "f2b6b1f0570fe18c8213e64c477a81d2")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7945042409:AAH5DenGtrLsrrfEjPzyQQju_UARa2ubrOY")
+ADMIN = int(os.getenv("ADMIN", "1782834874"))
+
+CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/DypixxTech")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002147456374"))
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002147456374"))
+
+DB_URI = os.getenv("DB_URI", "mongodb+srv://starcinebot:mkooaa@werdeveloper.vxfam.mongodb.net/?retryWrites=true&w=majority&appName=werdeveloper")
+DB_NAME = os.getenv("DB_NAME", "instalaoder")
+
+IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002421861644") # Add Multiple Channels iD By Space
+AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
