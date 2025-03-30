bfb1533e67c27efe9df9465847008c2dd94be258
Deep Coder <120001510+deep017i@users.noreply.github.com>
2025-03-30 07:53:20 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -5,40 +5,6 @@ from var import AUTH_CHANNELS, ADMIN
 from pyrogram.errors import RPCError
 from Script import txt
 
-# async def get_fsub(client, message):
-#     bot = client
-#     user_id = message.from_user.id
-#     not_joined = []
-#     for channel_id in AUTH_CHANNELS:
-#         try:
-#             member = await bot.get_chat_member(channel_id, user_id)
-#             if member.status == "kicked":
-#                 await message.reply("**🚫 You are banned from using this bot**",
-#                                     reply_markup=InlineKeyboardMarkup(
-#                                         [[InlineKeyboardButton("Support", user_id=int(ADMIN))]]
-#                                     ))
-#             if member.status in ["left", "restricted"]:
-#                 not_joined.append(channel_id)
-#         except RPCError:
-#             not_joined.append(channel_id)
-#     if not not_joined:
-#         return True
-#     buttons = []
-#     for index, channel_id in enumerate(not_joined, 1):
-#         try:
-#             chat = await bot.get_chat(channel_id)
-#             channel_link = chat.invite_link
-#             if not channel_link:
-#                 raise ValueError("No invite link available")
-#             buttons.append(InlineKeyboardButton(f"🔰 Channel {index} 🔰", url=channel_link))
-#         except Exception as e:
-#             print(f"Error fetching channel data: {e}")
-#     formatted_buttons = [buttons[i:i + 2] for i in range(0, len(buttons), 2)]
-#     await message.reply(txt.FORCE_SUBSCRIBE_TEXT.format(message.from_user.mention),
-#         reply_markup=InlineKeyboardMarkup(formatted_buttons)
-#     )
-#     return False
-
 async def get_fsub(client, message):
     dy = await client.get_me()
     bot = client
@@ -69,13 +35,53 @@ async def get_fsub(client, message):
         except Exception as e:
             print(f"Error fetching channel data: {e}")
     try_again_button = InlineKeyboardButton("🔄 Try Again", url=f"https://t.me/{dy.username}?start=start")
-    buttons.append(try_again_button)
-
-    formatted_buttons = [buttons[i:i + 2] for i in range(0, len(buttons), 2)]
-    
+    if len(buttons) > 0:
+        buttons.append(try_again_button)
+    else:
+        buttons = [try_again_button]
+    formatted_buttons = [buttons[i:i + 2] for i in range(0, len(buttons) - 1, 2)]
+    formatted_buttons.append([buttons[-1]])
     await message.reply(txt.FORCE_SUBSCRIBE_TEXT.format(message.from_user.mention),
                         reply_markup=InlineKeyboardMarkup(formatted_buttons))
     return False
+
+# async def get_fsub(client, message):
+#     dy = await client.get_me()
+#     bot = client
+#     user_id = message.from_user.id
+#     not_joined = []
+#     for channel_id in AUTH_CHANNELS:
+#         try:
+#             member = await bot.get_chat_member(channel_id, user_id)
+#             if member.status == "kicked":
+#                 await message.reply("**🚫 You are banned from using this bot**",
+#                                     reply_markup=InlineKeyboardMarkup(
+#                                         [[InlineKeyboardButton("Support", user_id=int(ADMIN))]]
+#                                     ))
+#             if member.status in ["left", "restricted"]:
+#                 not_joined.append(channel_id)
+#         except RPCError:
+#             not_joined.append(channel_id)
+#     if not not_joined:
+#         return True
+#     buttons = []
+#     for index, channel_id in enumerate(not_joined, 1):
+#         try:
+#             chat = await bot.get_chat(channel_id)
+#             channel_link = chat.invite_link
+#             if not channel_link:
+#                 raise ValueError("No invite link available")
+#             buttons.append(InlineKeyboardButton(f"🔰 Channel {index} 🔰", url=channel_link))
+#         except Exception as e:
+#             print(f"Error fetching channel data: {e}")
+#     try_again_button = InlineKeyboardButton("🔄 Try Again", url=f"https://t.me/{dy.username}?start=start")
+#     buttons.append(try_again_button)
+
+#     formatted_buttons = [buttons[i:i + 2] for i in range(0, len(buttons), 2)]
+    
+#     await message.reply(txt.FORCE_SUBSCRIBE_TEXT.format(message.from_user.mention),
+#                         reply_markup=InlineKeyboardMarkup(formatted_buttons))
+#     return False
 
 """
 This code is created and owned by @Dypixx. Do not remove or modify the credit.
