4fdfdc386b08b78b11e250fa973b3e41f5930e41
Deep Coder <120001510+deep017i@users.noreply.github.com>
2025-04-20 19:24:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ def show_github_html():
     return Response(response.text, mimetype='text/html') if response.status_code == 200 else ("Bot is LIVE", 500)
 if __name__ == "__main__":
     app.run()
+
+"""
+This code is created and owned by @Dypixx. Do not remove or modify the credit.
+
+Removing the credit does not make you a developer; it only shows a lack of respect for real developers.
+  
+Respect the work. Keep the credit.
+
+"""
