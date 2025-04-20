be5c7dd5013fdb49b0918350692c962041d47b79
Deep Coder <120001510+deep017i@users.noreply.github.com>
2025-04-20 13:50:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # © Coded by @Dypixx
 
-from flask import Flask
+from flask import Flask, Response
+import requests
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-    return 'Dypixx'
-
-
+def show_github_html():
+    response = requests.get('https://raw.githubusercontent.com/Dypixx/live/main/index.html')
+    return Response(response.text, mimetype='text/html') if response.status_code == 200 else ("Bot is LIVE", 500)
 if __name__ == "__main__":
     app.run()
